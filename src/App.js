10b3ab4e4842ebe52e65d59a3dd594c8e2b7ff18
Milind Badekar <59824517+manu-hacker@users.noreply.github.com>
2022-07-23 16:41:49 +0530

    Updated file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <h1 data-ns-test="project-name">Clone of IndiaMART</h1>
+      <p data-ns-test="project-description">
+        I want to clone the IndiaMART app
+      </p>
     </div>
   );
-}
+};
 
 export default App;
